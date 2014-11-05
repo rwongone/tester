3f81ea47f5ac75cbfec2f59cba6bf918053afb1a
Richard Wong <rk2wong@uwaterloo.ca>
2014-11-05 12:05:37 -0500

    good enough

--- tester.sh
+++ tester.sh
@@ -10,10 +10,12 @@ then
 		mkdir -p $2/tests
 		mkdir -p $2/testout
 		mkdir -p $2/refout
+		mkdir -p $2/diff
 	else
 		mkdir -p tests
 		mkdir -p testout
 		mkdir -p refout
+		mkdir -p diff
 	fi
 else
 	if [ $2 ]
@@ -26,10 +28,14 @@ else
 		$EXE < $file > ./testout/$(basename $file).out
 		if [ -f ./refout/$(basename $file).ref ]
 		then
-			echo diff ./testout/$(basename $file).out ./refout/$(basename $file).ref xxd
+			diff ./testout/$(basename $file).out ./refout/$(basename $file).ref | xxd > ./diff/$(basename $file).dif
+			diff ./testout/$(basename $file).out ./refout/$(basename $file).ref
+			cat ./diff/$(basename $file).dif | while read line
+			do
+				echo "a line: $line"
+			done
 		else
 			echo "Reference output for $(basename $file) does not exist in testout directory."
 		fi			
 	done
 fi
-
